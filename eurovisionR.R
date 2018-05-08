cbc3d8591f28da0064bca12e8c62ce6fdd032d5a
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-08 21:29:37 +0100

    Creating r shiny app and git repository

---
+++ eurovisionR.R
@@ -0,0 +1,53 @@
+library(shiny)
+countries <- paste(rep('generic european country', 26), LETTERS[1:26])
+
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+  
+  # App title ----
+  headerPanel("Eurovision party support generator: give the names of the people at your party, we'll tell you who they should support. The countries list will be updated once the finalists are announced!"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(
+    # Input: Selector for variable to plot against mpg ----
+    sliderInput("n_countries", "Number of countries per viewer:",
+                min = 1, max = 5,
+                value = 1),
+      textInput('viewers', 'Enter the viewers names separated by commas', "Chad, Eric, Donald, Huw", width = '800px')
+    
+  ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel(
+    
+    DT::dataTableOutput("mytable")
+    
+  )
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+    
+    output$mytable <- DT::renderDataTable({
+      viewers <- strsplit(input$viewers, split = ',')[[1]]
+      viewers <- gsub(' ', '', viewers)
+
+      rooting_for <- list()
+      for(i in 1:length(viewers)){
+        viewer <- viewers[i]
+        nums <- sample(1:length(countries), input$n_countries)
+        rooting_for[[viewer]] <- countries[nums]
+        countries <- countries[-nums]
+      }
+      
+      roots_df <- as.data.frame(rooting_for)
+      
+      print(roots_df)
+    })
+    
+    
+  
+}
+
+shinyApp(ui, server)
